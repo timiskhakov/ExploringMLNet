dfdf3a619f84c54a1e10dbccdd4e03ac9a24d216
Tim Iskhakov <timiskhakov@gmail.com>
2019-08-14 21:00:28 +0300

    Update input model

--- ExploringMLNet.Expression/Models/Input.cs
+++ ExploringMLNet.Expression/Models/Input.cs
@@ -5,10 +5,16 @@ namespace ExploringMLNet.Models
     public class Input
     {
         [LoadColumn(0)]
-        public float First;
+        public float A;
         [LoadColumn(1)]
-        public float Second;
+        public float B;
         [LoadColumn(2)]
+        public float C;
+        [LoadColumn(3)]
+        public float D;
+        [LoadColumn(4)]
+        public float E;
+        [LoadColumn(5)]
         public float Result;
     }
 }
